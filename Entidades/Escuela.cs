201f7c7d8b52f66c35bc7974afad384b90d5df56
Dairo Portillo <dairo.jose17@hotmail.com>
2020-10-31 16:42:11 -0500

    step 2

---
+++ Entidades/Escuela.cs
@@ -0,0 +1,62 @@
+namespace CoreEscuela.Entidades
+{
+    class Escuela
+    {
+        string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public int AñoDeCreacion { get; set; }
+
+        public string Pais { get; set; }
+        public string Ciudad { get; set; }
+
+        public TiposEscuela TipoEscuela { get; set; }
+
+        public Curso[] Cursos { get; set; }
+
+        //constructor
+        public Escuela(string nombreConstructor, int año)
+        {
+            this.nombre = nombreConstructor;
+            this.AñoDeCreacion = año;
+        }
+        //segundo constructor con parámetros opcionales
+        public Escuela(string nombreConstructor, int año, TiposEscuela tipo, string pais = "", string ciudad = "")
+        {
+            this.nombre = nombreConstructor;
+            this.AñoDeCreacion = año;
+            this.TipoEscuela = tipo;
+            this.Pais = pais;
+            this.Ciudad = ciudad;
+        }
+
+        //sobreescribir el método tostring del objeto
+        public override string ToString() => ($"Nombre: {Nombre}, Tipo: {TipoEscuela}, \nPaís: {Pais}, Ciudad: {Ciudad}");
+
+        public void imprimirCursos()
+        {
+
+            try
+            {
+                System.Console.WriteLine("==================================");
+                System.Console.WriteLine($"Cursos de la Escuela {this.nombre}");
+                System.Console.WriteLine("==================================");
+                
+                foreach (var Curso in this.Cursos)
+                {
+                    System.Console.WriteLine($"Nombre: {Curso.Nombre}, Id: {Curso.UniqueId}, Jornada: {Curso.Jornada}");
+                }
+            }
+            catch (System.Exception)
+            {
+
+                System.Console.WriteLine("Escuela no tiene cursos");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,23 +1,27 @@
 ﻿using System;
+using CoreEscuela.Entidades;
 
 namespace coreEscuela
 {
-    class Escuela
-    {
-        string nombre;
-        string direccion;
-        int añoFundacion;
-
-        void Timbrar(){
-            Console.Beep(10000, 3000);
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
+            var escuela = new Escuela("Dap Academy", 2020, TiposEscuela.Primaria, ciudad: "Aguachica");
+            escuela.Pais = "Canadá";
+            escuela.Ciudad = "Toronto";
+            escuela.TipoEscuela = TiposEscuela.Secundaria;
+
+            // Curso[] arregloCursos = {
+            //     new Curso {Nombre = "101"},
+            //     new Curso {Nombre = "201"},
+            //     new Curso {Nombre = "301"}
+            // };
+
+            // escuela.Cursos = arregloCursos;
+
+            escuela.imprimirCursos();
         }
     }
 }
