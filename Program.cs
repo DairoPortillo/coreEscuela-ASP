218276076282b0218d5a692a897372ab3a7a1e0f
Dairo Portillo <dairo.jose17@hotmail.com>
2020-10-31 14:50:21 -0500

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace coreEscuela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
